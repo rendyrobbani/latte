0bacb938f95110b3b9c0b720d799091ba1a1c315
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-07 18:48:19 +0800

    Latte

--- src/main/java/com/rendyrobbani/common/classification/urusan/BidangClassification.java
+++ src/main/java/com/rendyrobbani/common/classification/urusan/BidangClassification.java
@@ -2,7 +2,7 @@
 
 public interface BidangClassification extends UrusanClassification {
 
-	String VALUE = "(?!0.*|.\\.00.*|X\\.\\d.*|\\d\\.XX.*$)(X|\\d)\\.(XX|\\d{2})";
+	String VALUE = "(?!0.*|.\\.0{2}|X\\.\\d{2}|\\d\\.X{2}$)(X|\\d)\\.(XX|\\d{2})";
 	String REGEX = "^" + VALUE + "$";
 
 	String getBidangCode();
